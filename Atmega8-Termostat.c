77c1459e2241b4240a69b7329142c060a0495d6a
zeq52giw <87485100+illya334@users.noreply.github.com>
2022-07-24 14:00:36 +0300

    Add files via upload

--- Atmega8-Termostat.c
+++ Atmega8-Termostat.c
@@ -12,16 +12,20 @@
 #include <util/delay.h>
 #include <math.h>
 
+#define Vcc		5		// Input voltage
+#define R1		10000	// R1 in Ohms
+#define Vref	Vcc		// Reference voltage for ADC
+
 #define BUTT_PRESSED_CNT	5
 #define BUTT_HELD_CNT		20
 #define BUTT_HELD_DELAY		70
 
 #define TCS 256 // should be ~10ms
 /*
-	РџРѕРґРѕР±СЂР°С‚СЊ РґРµР»РёС‚РµР»СЊ РјРѕР¶РЅРѕ РїРѕ С„РѕСЂРјСѓР»Рµ: 256 * k / f = t (sec)
-	k - РґРµР»РёС‚РµР»СЊ (8, 64, 256, 1024).
-	f - С‡Р°СЃС‚РѕС‚Р° (Hz)
-	t - РІСЂРµРјСЏ (sec)
+	Подобрать делитель можно по формуле: 256 * k / f = t (sec)
+	k - делитель (8, 64, 256, 1024).
+	f - частота (Hz)
+	t - время (sec)
 
 	   f   |   k   |    t
 	 1 MHz |   64  |  ~0.016
@@ -34,6 +38,8 @@
 	12 MHz | 1024  |  ~0.021
 	16 MHz | 1024  |  ~0.016
 */
+
+#define MENU_TIME 5 // delay in seconds
 
 #if (TCS == 8)
 	#define _TCCR0_def 2;
@@ -56,87 +62,44 @@
 #define false 0
 
 // for readADC
-#define T1 1 // РўРµСЂРјРѕСЂРµР·РёСЃС‚РѕСЂ 1
-#define T2 3 // РўРµСЂРјРѕСЂРµР·РёСЃС‚РѕСЂ 2
-#define TNONE 100 // РІС‹РІРѕРґ С‚РёСЂРµ
 
-byte T2MAX = -9; // РњР°РєСЃРёРјР°Р»СЊРЅР°СЏ С‚РµРјРїРµСЂР°С‚СѓСЂР° Р±РѕР№Р»Р»РµСЂР°
+#define ADC0 0
+#define ADC1 1
+
+#define T1_ADC ADC0
+#define T2_ADC ADC1
+
+// for WriteDisplay
+#define T1_Print	1	// Termoresistor 1 (Кател)
+#define T2_Print	3	// Termoresistor 2 (Бойлер)
+#define TNONE_Print 100 // print '-'
+
+byte T2MAX = 60; // Максимальная температура бойллера
 
 // https://www.gotronic.fr/pj2-mf52type-1554.pdf
-const ushort ConstRes[] = { // СЃРѕРїСЂРѕС‚РёРІР»РµРЅРёРµ С‚РµСЂРјРёСЃС‚РѕСЂР° (С‚Р°Р±Р»РёС†Р°), РІ РѕРјР°С…
-	181700, // -30
-	133300, // -25
-	 98880, // -20
-	 74100, // -15
-	 56060, // -10
-	 42800, //  -5
-	 98960, //   0
-	 25580, //   5
-	 20000, //  10
-	 15760, //  15
-	 12510, //  20
-	 10000, //  25 (This)
-	  8048, //  30
-	  6518, //  35
-	  5312, //  40
-	  4354, //  45
-	  3588, //  50
-	  2974, //  55
-	  2476, //  60
-	  2072, //  65
-	  1743, //  70
-	  1473, //  75
-	  1250, //  80
-	  1065, //  85
-	   911, //  90
-	   782, //  95
-	   674, // 100
-	   583, // 105
-	   506, // 110
-};
-
-struct stNowResTemp{
-	byte	Temp1;
-	ushort	R1;
-	
-	byte	Temp2;
-	ushort	R2;
-	
-	byte	Temp3;
-	ushort	R3;
-}  NowResTemp;
 
 // https://ru.wikipedia.org/wiki/%D0%A3%D1%80%D0%B0%D0%B2%D0%BD%D0%B5%D0%BD%D0%B8%D0%B5_%D0%A1%D1%82%D0%B5%D0%B9%D0%BD%D1%85%D0%B0%D1%80%D1%82%D0%B0_%E2%80%94_%D0%A5%D0%B0%D1%80%D1%82%D0%B0
-// return Temterature
-double ntcResult(double nowR, double Temp1, double R1, double Temp2, double R2, double Temp3, double R3){
-	
-	double 	L1 = log(R1),
-			L2 = log(R2),
-			L3 = log(R3);
-			
-	double 	Y1 = 1/Temp1,
-			Y2 = 1/Temp2,
-			Y3 = 1/Temp3;
-			
-	double 	h2 = (Y2 - Y1) / (L2 - L1),
-			h3 = (Y3 - Y1) / (L3 - L1);
-			
-	double 	C = ((h3 - h2) / (L3 - L2)) * (1 / (L1 + L2 + L3)),
-			B = h2 - C * (L1*L1 + L1*L2 + L2*L2),
-			A = Y1 - (B + L1*L1*C) * L1;
-	
-	double tmp = log(nowR);
-	
-	return 1/(A + B * tmp + C * (tmp*tmp*tmp) );
+
+const double	ntcA = 0.001120098911110, // For formula (GetTempByVolt)
+				ntcB = 0.000236277875190,
+				ntcC = 0.000000073729442;
+
+byte GetTempByVolt( float Volt ){
+	double ln = log( (R1*Volt)/(Vcc - Volt) );
+	return (byte)( round(1/( ntcA + ntcB * ln + ntcC * (ln*ln*ln) ) - 273.15) );
+}
+
+float GetVoltByADC( ushort ADC_IN ){
+	return (float)(ADC_IN) * Vref / 1024;
 }
 
 ushort readADC( ubyte port ){
 	// ONLY T1, T2 parameters
 	
-	ADMUX = port;
-	ADCSRA = 0xC6;
+	ADMUX = (1 << REFS0) | port;
+	ADCSRA = (1 << ADEN) | (1 << ADSC);
 	
-	while( ADCSRA & ADSC );
+	while( ADCSRA & (1 << ADSC) );
 	
 	return ADC;
 }
@@ -144,16 +107,7 @@ ushort readADC( ubyte port ){
 // Not Use, this for WriteDisplay.
 void _DisplayOn( ubyte type, ubyte value ){
 	
-	/*
-		type = 0 - DOP menu (play, pause, USB, SD, MHz, MP3, Points).
-			value = 0 - play.
-				  = 1 - pause.
-				  = 2 - USB.
-				  = 3 - SD.
-				  = 4 - MHz.
-				  = 5 - MP3.
-				  = 6 - Points.
-				  
+	/*	  
 		type = 1 - first 7-segment display
 			( value < 10 ) if value > 9 is '-'
 		type = 2 - second 7-segment display
@@ -165,35 +119,6 @@ void _DisplayOn( ubyte type, ubyte value ){
 	//PORTD = 0x7F;
 	
 	//_delay_us(10);
-
-	if( type == 0 ){
-		
-		switch(value){
-			case 0:
-				PORTD = ~0x1;
-			break;
-			case 1:
-				PORTD = ~0x2;
-			break;
-			case 2:
-				PORTD = ~0x4;
-			break;
-			case 3:
-				PORTD = ~0x8;
-			break;
-			case 4:
-				PORTD = ~0x20;
-			break;
-			case 5:
-				PORTD = ~0x40;
-			break;
-			case 6:
-				PORTD = ~0x10;
-			break;
-		}
-		
-		return;
-	}
 	
 	switch(type){
 		case 1:
@@ -212,8 +137,8 @@ void _DisplayOn( ubyte type, ubyte value ){
 	
 	
 	
-	// РќРµ РїСЂР°РІРёР»СЊРЅРѕ РєРѕРјРїРёР»РёС†Р° - default СЂР°Р±РѕС‚Р°РµС‚ РІСЃРµРіРґР°(
-	if(value > 9){ // РІРјРµСЃС‚Рѕ default
+	// Не правильно компилица - default работает всегда(
+	if(value > 9){ // вместо default
 		PORTD = ~0x40;
 		_delay_us(100);
 	} else {
@@ -351,51 +276,34 @@ void _DisplayOn( ubyte type, ubyte value ){
 	PORTD = 0x7F; // clear display
 }
 
-// Р’С‹РІРѕРґ РЅР° РґРёСЃРїР»РµР№
+// Print on display
 void WriteDisplay( ubyte type, byte value ){
-	/*
-		type = 0 - DOP menu (play, pause, USB, SD, MHz, MP3, Points).
-			value = 0 - play.
-				  = 1 - pause.
-				  = 2 - USB.
-				  = 3 - SD.
-				  = 4 - MHz.
-				  = 5 - MP3.
-				  = 6 - Points.
-				  
+	/*	  
 		type = 1 - first temperature (T1).
 			value = temperature (if value > 99 is "--", or value < -9 is "--").
 		type = 2 - second temperature (T2).
 	*/
-	
-	if( type != 0 ){
-		
-		if( value > 99 || value < -9 ){
-			_DisplayOn( type, 10 );
-			_DisplayOn( type+1, 10 );
-		} else if( value < 0 ){
-			value = -value;
-			_DisplayOn( type, TNONE );
-			_DisplayOn( type+1, value );
-		} else {
-			_DisplayOn( type, value / 10 );
-			_DisplayOn( type+1, value % 10 );
-		}
-	} else
-		_DisplayOn( type, value );
+	if( value > 99 || value < -9 ){
+		_DisplayOn( type, 10 );
+		_DisplayOn( type+1, 10 );
+	} else if( value < 0 ){
+		value = -value;
+		_DisplayOn( type, TNONE_Print );
+		_DisplayOn( type+1, value );
+	} else {
+		_DisplayOn( type, value / 10 );
+		_DisplayOn( type+1, value % 10 );
+	}
 }
 
-void Get3Res(ushort nowR){
-	
-}
+byte TemperatureT1 = 0;
+byte TemperatureT2 = 0;
 
-byte GetT1(){
-	
-	
-	
-	return (byte) round( ntcResult(0, 0, 0, 0, 0, 0, 0) );
-	
-}
+bool T2MAX_MANU_ON = false;
+
+bool RELEYON = false;
+
+bool CanReleyOn = true;
 
 int main(void){
 	
@@ -417,11 +325,60 @@ int main(void){
 	// ====== CODE ======
 	
     while(1){
-		WriteDisplay(T1, T2MAX);
-		WriteDisplay(T2, TNONE);
+		DDRB |= (1 << PB6);
+		PORTB |= (1 << PB6);
+		
+		DDRD |= (1<<PD0);
+		PORTD | (1<<PD0);
+		
+		if( T2MAX_MANU_ON == true ){
+			
+			WriteDisplay(T2_Print, T2MAX);
+			
+		} else {
+			WriteDisplay(T1_Print, TemperatureT1);
+			
+			WriteDisplay(T2_Print, TemperatureT2);	
+		}
+		
+		if( RELEYON == true ){
+			
+			// DISPLAY -> PLAY = ON
+			DDRB |= (1 << PB6);
+			PORTB |= (1 << PB6);
+			
+			DDRD |= (1 << PD0);
+			PORTD &= ~(1 << PD0);
+			
+			// RELEY = ON
+			DDRC |= (1 << PC5);
+			PORTC |= (1 << PC5);
+			
+			_delay_us(100);
+		} else {
+			PORTC &= ~(1 << PC5);
+		}
+		
+		if( (TemperatureT1 > TemperatureT2 + 10) && (CanReleyOn == true) && ( RELEYON == false ) )
+			RELEYON = true;
+			
+		if( (TemperatureT1 < TemperatureT2 + 3) && (CanReleyOn == true) )
+			RELEYON = false;
+			
+			
+			
+		if( (TemperatureT2 > T2MAX) && (CanReleyOn == true) ){
+			RELEYON = false;
+			CanReleyOn = false;
+		}
+		
+		if( TemperatureT2 < T2MAX - 5 ) CanReleyOn = true;
+		
 	}
 }
 
+
+// Handle buttons and ADC, T2MAX_menu
 
 ubyte BUTT_counter1_plus  = 0;
 ubyte BUTT_counter2_minus = 0;
@@ -435,18 +392,25 @@ bool BUTT_WAS_HELD2	   = false;
 ubyte BUTT_counter1_held = 0;
 ubyte BUTT_counter2_held = 0;
 
+ubyte TADCREAD = 0;
+
+ushort menu_counter = 0;
+
 ISR(TIMER0_OVF_vect){
 	DDRB  &= ~3;
 	PORTB |= 3;
 	
+	// ======================== BUTTON 2 (+) =======================
 	if( !(PINB & 1) ){ // Button + was pressed
+		menu_counter = 0;
+		
 		if(BUTT_counter1_plus < 255)
 			BUTT_counter1_plus++;
 			
 		if(BUTT_counter1_plus > BUTT_PRESSED_CNT && !BUTT_WAS_PRESSED1){ // MS_PRESSED
 			BUTT_WAS_PRESSED1 = true;
 			
-			
+			T2MAX_MANU_ON = true;
 		}
 		
 		if( BUTT_counter1_plus > BUTT_HELD_CNT ){ // MS_HELD
@@ -464,17 +428,17 @@ ISR(TIMER0_OVF_vect){
 		if( (BUTT_WAS_PRESSED1 == true) && (!BUTT_WAS_HELD1)  ){ // MS_RELEASED without held
 			
 			T2MAX++;
-			
 		}
-		BUTT_WAS_PRESSED1 = false;
-		BUTT_WAS_HELD1 = false;
-		BUTT_counter1_plus = 0;
-		BUTT_counter1_held = 0;
+		BUTT_WAS_PRESSED1	= false;
+		BUTT_WAS_HELD1		= false;
+		BUTT_counter1_plus	= 0;
+		BUTT_counter1_held	= 0;
 	}
 	
-	// =============== BUTTON 2 (-) =======================
+	// ======================== BUTTON 2 (-) =======================
 	
 	if( !(PINB & 2) ){ // Button - was pressed
+		menu_counter = 0;
 		
 		if(BUTT_counter2_minus < 255)
 			BUTT_counter2_minus++;
@@ -482,7 +446,7 @@ ISR(TIMER0_OVF_vect){
 		if(BUTT_counter2_minus > BUTT_PRESSED_CNT && !BUTT_WAS_PRESSED2){ // MS_PRESSED
 			BUTT_WAS_PRESSED2 = true;
 			
-			
+			T2MAX_MANU_ON = true;
 		}
 		
 		if( BUTT_counter2_minus > BUTT_HELD_CNT ){ // MS_HELD
@@ -501,9 +465,33 @@ ISR(TIMER0_OVF_vect){
 			T2MAX--;
 			
 		}
-		BUTT_WAS_PRESSED2 = false;
-		BUTT_WAS_HELD2 = false;
-		BUTT_counter2_minus = 0;
-		BUTT_counter2_held = 0;
+		BUTT_WAS_PRESSED2	= false;
+		BUTT_WAS_HELD2		= false;
+		BUTT_counter2_minus	= 0;
+		BUTT_counter2_held	= 0;
 	}
+	
+	// =================== ADC =================
+	
+	if( TADCREAD > 100 ){ // ~1 second
+		TADCREAD = 0;
+		
+		TemperatureT1 = GetTempByVolt(GetVoltByADC(readADC(T1_ADC)));
+		if( TemperatureT1 < 5 ) TemperatureT1 = TNONE_Print;
+		
+		TemperatureT2 = GetTempByVolt(GetVoltByADC(readADC(T2_ADC)));
+		if( TemperatureT2 < 5 ) TemperatureT2 = TNONE_Print;
+		
+	} else TADCREAD++;
+	
+	// ======================== MENU =====================
+	
+	if( T2MAX_MANU_ON == true )
+		menu_counter++;
+		
+	if( menu_counter > MENU_TIME*100 ){
+		T2MAX_MANU_ON = false;
+		menu_counter = 0;
+	}
+	
 }
